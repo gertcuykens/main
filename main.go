e7ff94704225336ffbf3ad1178387b107a166f19
Gert Cuykens <gert.cuykens@gmail.com>
2019-10-22 05:39:35 +0200

    _

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/rpc"
+	"os"
+
+	cakebaker "github.com/gertcuykens/module"
+)
+
+func main() {
+	srv := rpc.NewServer()
+	srv.Register(new(cakebaker.CakeBaker))
+
+	go func(srv *rpc.Server) {
+		l, err := net.Listen("tcp", "localhost:8080")
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		defer l.Close()
+
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				continue
+			}
+			go func(conn net.Conn) {
+				srv.ServeConn(conn)
+				defer conn.Close()
+			}(conn)
+		}
+	}(srv)
+
+	client, err := rpc.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	var reply string
+	err = client.Call("CakeBaker.BakeIt", 5, &reply)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer client.Close()
+	fmt.Printf("%s\n", reply)
+}
